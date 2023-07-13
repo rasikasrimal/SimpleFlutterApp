5f48b974118911ab0193df55b42b8f42098ecc99
rasikasrimal <rasikacooldj620@gmail.com>
2023-07-13 21:30:52 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:second_app/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'My App',
+      home: HomePage(),
+    );
+  }
+}
